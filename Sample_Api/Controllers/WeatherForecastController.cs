cab09910af16b75e7c80035d77bb62d1e3a2ccf9
Rahul Parab <rahul.parab@qualfon.com>
2025-06-06 13:14:57 +0530

    removed last updated files

--- Sample_Api/Controllers/WeatherForecastController.cs
+++ Sample_Api/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Sample_Api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Scorching2"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
